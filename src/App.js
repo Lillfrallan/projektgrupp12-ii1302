322b6508f8ea4f86f44c3762a08f7c159a9fa332
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-04-08 11:58:29 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      HEY HEY HEY tttteeest !??!?!
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://ultimateqa.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-testid="learn-link"
+        >
+          Learn Testing and dev
+        </a>
+      </header>
     </div>
   );
 }
