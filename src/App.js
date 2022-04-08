06e96216f4046ca3c07755b92360ecb94e1fb30d
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-04-08 12:43:57 +0200

    website is up and running

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
+      
           WEBSITE IS UP AND RUNNING
-        </p>
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          data-testid="learn-link"
-        >
-
-        </a>
-      </header>
+     
     </div>
   );
 }
