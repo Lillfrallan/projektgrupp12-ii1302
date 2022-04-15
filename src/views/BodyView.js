8807d3ca29f0c9b5076aff1435d61ba60adf5ec1
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-04-15 14:18:05 +0200

    bugg searching

--- src/views/BodyView.js
+++ src/views/BodyView.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import './Body.css'
-// import * as api_client from '../services/api_client'
+import * as api_client from '../services/api_client'
 
-function BodyView() { //blobImages, blobDateTime
+function BodyView( {blobImages} ) { 
 
     // console.log(blobDateTime)
 
     return (
         <div>
             HEEEEEEEY
-            {/* {blobImages.map( (blobImages) => (
+            {blobImages.map( (blobImages) => (
                 <div>
                     <div className="images">
                         <img
-                            key={blobImages.id}
                             src={api_client.get_image_url(blobImages)}
                             alt=""
                         /> 
                     </div>
                     
                 </div>
-            ))}  */}
+            ))} 
         </div>
         )
 }
