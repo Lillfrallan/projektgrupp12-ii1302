43e4ffce6117c3ca56364333156f9db518c72175
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-04-21 20:06:44 +0200

    webpack done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
+const { BlobServiceClient } = require("@azure/storage-blob");
 
 
 const rootElement = document.getElementById('root');
