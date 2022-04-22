d7ffffaa7e3e77c148d6ce32d7523a95019bb2e6
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-04-22 11:16:54 +0200

    finally solved

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const { BlobServiceClient } = require("@azure/storage-blob");
 //required to be here for github actions to work -> not used
 const blobServiceClient = new BlobServiceClient(`https://ktodb.blob.core.windows.net`);
 
-
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
