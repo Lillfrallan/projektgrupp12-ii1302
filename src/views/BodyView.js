464d872b884fd64e7170bc5931008b193ddeaa37
Malcolm <malcolm.liljedahl94@gmail.com>
2022-04-26 16:34:16 +0200

    test

--- src/views/BodyView.js
+++ src/views/BodyView.js
@@ -7,6 +7,7 @@ function BodyView({ images, datesAndTime, index, redirect, blobs, name }) {
     
     return (
         <div className="bodyView">
+            <div>HEEEEEEJ ROBIN</div>
             <div className="blobs" onClick={() => redirect(blobs)}>
                     <img className="image" src={images} alt=""/>
                     <div className="dateAndTime">{datesAndTime}</div>
