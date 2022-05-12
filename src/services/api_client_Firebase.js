be088f9288b73cd25db13c164f8402263ec77150
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-05-12 22:31:24 +0200

    secrets updated

--- src/services/api_client_Firebase.js
+++ src/services/api_client_Firebase.js
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage, deleteObject, ref, getDownloadURL } from "firebase/storage";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+import {API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID} from './scripts'
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAVj7INM7guffGsZhXLNSydDBEmYakAQLk",
-    authDomain: "projectgroup12-2f2a2.firebaseapp.com",
-    projectId: "projectgroup12-2f2a2",
-    storageBucket: "projectgroup12-2f2a2.appspot.com",
-    messagingSenderId: "102821745364",
-    appId: "1:102821745364:web:87c46aaaf783ce42016141",
-    measurementId: "G-X3VRVLMJPS"
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId:  PROJECT_ID,
+    storageBucket:  STORAGE_BUCKET,
+    messagingSenderId:  MESSAGING_SENDER_ID,
+    appId:  APP_ID,
+    measurementId: MEASUREMENT_ID
 };
 
 const get_image_url = (IMAGE) => {
@@ -21,6 +24,8 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const storage = getStorage();
+const auth = getAuth(app);
+
 
 const delete_image = (IMAGE) => {
     const deleteRef = ref(storage, `images/${IMAGE}`)
@@ -44,4 +49,19 @@ const download_image = (IMAGE) => {
 }
 
 
-export  {get_image_url, delete_image, download_image,  firebaseConfig, app, analytics, storage, getDownloadURL, ref} 
+export  {
+    get_image_url, 
+    delete_image, 
+    download_image,  
+    firebaseConfig, 
+    app, 
+    analytics, 
+    storage, 
+    getDownloadURL, 
+    ref,
+    auth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut
+} 
