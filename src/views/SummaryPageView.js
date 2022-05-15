ff2cb1095cafe5ee6e2b418075c920b2b5f2e860
Lillfrallan <76948186+Lillfrallan@users.noreply.github.com>
2022-05-15 21:07:14 +0200

    Css fixes

--- src/views/SummaryPageView.js
+++ src/views/SummaryPageView.js
@@ -16,42 +16,42 @@ function SummaryView( {
             <div className="summaryDisplay">
                 <div className="summaryImageContainer">
                     <img className="summaryImage" src={images} alt=""/>
-                </div>
-                <div className="summaryData">
-                    <div>Blob name: <div className="data">{name}</div></div>
-                    <div>Etag: 
-                        <div className="data">{etag}</div>
-                    </div>
-                    <div>Bucket: 
-                        <div className="data">{bucket}</div>
-                    </div>
-                    <div>Storage Class: 
-                        <div className="data">{storageClass}</div>
-                    </div>
-                    <div>Content Encoding: 
-                        <div className="data">{contentEncoding}</div>
-                    </div>
-                    <div>Content Type: 
-                        <div className="data">{contentType}</div>
-                    </div>
-                    <div>Crc32c: 
-                        <div className="data">{crc32c}</div>
-                    </div>
-                    <div>Generation: 
-                        <div className="data">{generation}</div>
-                    </div>
-                    {/* <div>Meta Generation: 
-                        <div className="data">{metageneration}</div>
-                    </div> */}
-                    <div>Size: 
-                        <div className="data">{size}</div>
-                    </div>
-                    <div>md5Hash: 
-                        <div className="data">{md5Hash}</div>
-                    </div>
-                    <div>Date and Time: 
-                        <div className="data">{datesAndTime}</div>
-                    </div>
+                    <span className="summaryData">
+                        <div>Blob name: <div className="data">{name}</div></div>
+                        <div>Etag: 
+                            <div className="data">{etag}</div>
+                        </div>
+                        <div>Bucket: 
+                            <div className="data">{bucket}</div>
+                        </div>
+                        <div>Storage Class: 
+                            <div className="data">{storageClass}</div>
+                        </div>
+                        <div>Content Encoding: 
+                            <div className="data">{contentEncoding}</div>
+                        </div>
+                        <div>Content Type: 
+                            <div className="data">{contentType}</div>
+                        </div>
+                        <div>Crc32c: 
+                            <div className="data">{crc32c}</div>
+                        </div>
+                        <div>Generation: 
+                            <div className="data">{generation}</div>
+                        </div>
+                        {/* <div>Meta Generation: 
+                            <div className="data">{metageneration}</div>
+                        </div> */}
+                        <div>Size: 
+                            <div className="data">{size + " bytes"}</div>
+                        </div>
+                        <div>md5Hash: 
+                            <div className="data">{md5Hash}</div>
+                        </div>
+                        <div>Date and Time: 
+                            <div className="data">{datesAndTime}</div>
+                        </div>
+                    </span>
                 </div>
             </div>
             <div className="summaryButtonContainer">
