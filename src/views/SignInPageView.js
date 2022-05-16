40c0870adb9ac407f68830243d0d37e27e98e92c
nahom solomon <nkso@kth.se>
2022-05-16 11:21:28 +0200

    signin and signout

--- src/views/SignInPageView.js
+++ src/views/SignInPageView.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './css/SignIn.css'
+import {Link} from 'react-router-dom'
 
 function SignInPageView( {setLoginPassword, setLoginEmail, loginUser} ) {
         
     return (
         <div className="signInContainer">
             <div className="signInLoayout">
-                <h3>Sign in</h3>
-                <input placeholder="Enter an email" onChange={(event) => {setLoginEmail(event.target.value)}}/>
-                <input placeholder="Enter a password" onChange={(event) => {setLoginPassword(event.target.value)}}/>
-                <button onClick={loginUser} title="login">Log in</button>
+                <h3>Sign In</h3>
+                <input placeholder="Enter an email" className ="email" onChange={(event) => {setLoginEmail(event.target.value)}}/>
+                <br></br>
+                <input className ="password" type={'password'} placeholder="Enter a password" onChange={(event) => {setLoginPassword(event.target.value)}}/>
+                <br></br>
+                <button className='signUpButton'  onClick={loginUser} title="login">Log in</button>
+                <br></br>
+                <Link to='/signUpUser' className='link'>
+                    <span>New user? Register here</span>
+                </Link>
             </div>
             
         </div>
--- src/views/SignUpPageView.js
+++ src/views/SignUpPageView.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function SignUpPageView( {setRegisterPassword , setRegisterEmail, registerUser }) {
     
     return (
-        <div >
-            <h3>Sign up.</h3>
-                <input placeholder="Enter an email" onChange={(event) => {setRegisterEmail(event.target.value)}}/>
-                <input placeholder="Enter a password" onChange={(event) => {setRegisterPassword(event.target.value)}}/>
-
-                <button onClick={registerUser}>Sign up</button>
-
+        <div className="signInContainer">
+            <div className="signInLoayout">
+                <h3>Sign Up</h3>
+                <input placeholder="Enter an email" className ="email" onChange={(event) => {setRegisterEmail(event.target.value)}}/>
+                <br></br>
+                <input className ="password" placeholder="Enter a password" onChange={(event) => {setRegisterPassword(event.target.value)}}/>
+                <br></br>
+                <button className='signUpButton'  onClick={registerUser} >Log in</button>
+                <br></br>
+                <Link to='/signInUser' className='link'>
+                    <span>Already Registered? Log in here</span>
+                </Link>
+            </div>
+            
         </div>
     )
 }
