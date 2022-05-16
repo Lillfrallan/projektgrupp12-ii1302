1a77fb6dcc5d8647fe93b8e1d8f37770c1e226f2
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-05-16 10:23:43 +0200

    test

--- src/views/SignInPageView.js
+++ src/views/SignInPageView.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import './css/SignIn.css'
 
 function SignInPageView( {setLoginPassword, setLoginEmail, loginUser} ) {
         
     return (
-        <div>
-            <h3>Sign in</h3>
+        <div className="signInContainer">
+            <div className="signInLoayout">
+                <h3>Sign in</h3>
                 <input placeholder="Enter an email" onChange={(event) => {setLoginEmail(event.target.value)}}/>
                 <input placeholder="Enter a password" onChange={(event) => {setLoginPassword(event.target.value)}}/>
-
                 <button onClick={loginUser} title="login">Log in</button>
+            </div>
             
         </div>
     )
--- src/views/SignUpPageView.js
+++ src/views/SignUpPageView.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SignUpPageView( {setRegisterPassword , setRegisterEmail, registerUser }) {
     
     return (
-        <div>
+        <div >
             <h3>Sign up.</h3>
                 <input placeholder="Enter an email" onChange={(event) => {setRegisterEmail(event.target.value)}}/>
                 <input placeholder="Enter a password" onChange={(event) => {setRegisterPassword(event.target.value)}}/>
