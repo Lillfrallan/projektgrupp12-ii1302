f11cad3e4f07ef5d2a101d873b62b2af4df7f659
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-05-17 20:58:10 +0200

    final

--- src/views/SignUpPageView.js
+++ src/views/SignUpPageView.js
@@ -6,7 +6,7 @@ function SignUpPageView( {setRegisterPassword , setRegisterEmail, registerUser }
     return (
         <div className="signInContainer">
             <div className="signInLoayout">
-                <h3>Sign Up</h3>
+                <h3 className="signintitle" >Sign Up</h3>
                 <input placeholder="Enter an email" className ="email" onChange={(event) => {setRegisterEmail(event.target.value)}}/>
                 <br></br>
                 <input className ="password" placeholder="Enter a password" onChange={(event) => {setRegisterPassword(event.target.value)}}/>
